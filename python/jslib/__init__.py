9aee819715f94917fa021556436fa93a7df39bde
wangyijian <wangyijian0525@gmail.com>
2017-06-12 17:25:59 +0800

    [MOD] python ADD math_matrix

--- python/jslib/__init__.py
+++ python/jslib/__init__.py
@@ -1,9 +1,11 @@
 ﻿# -*- coding:utf-8 -*-
 
 import json
-import jslib.dateTime
+import jslib.date_time
+import jslib.math_matrix
 
 def get_versions():
     dict_versions = {}
-    dict_versions['dateTime'] = jslib.dateTime.get_versions()
+    dict_versions['dateTime'] = jslib.date_time.get_versions()
+    dict_versions['math_matrix'] = jslib.math_matrix.get_versions()
     return dict_versions
---
+++ python/jslib/math_matrix.py
@@ -0,0 +1,44 @@
+﻿# -*- coding:utf-8 -*-
+# Create On 20170606
+# Auth: wang.yijian
+# desc: 数学计算 - 矩阵
+
+import pandas as pd
+import json
+
+_version_jason = '''
+{
+    "version": "0.0.1"
+}
+''' # END VERSION_JSON
+
+def get_versions():
+    return json.loads(_version_json)
+
+# ------------------------------------------------------------------------
+def sum_diagonal(input_matrix):
+    '''
+    @introduction: 计算对角线之和。输入必须为 N*N 的矩阵
+    @param:
+        input_matrix:   [pandas.DataFrame] N*N 矩阵
+    @return:
+        sum_value:      [float] 对角线之和
+    '''
+    rowcount = len(input_matrix)
+    if rowcount <= 0:
+        return None
+    colcount = len(input_matrix.iloc[0])
+    if colcount <= 0 or colcount != rowcount:
+        return None
+
+    sum_value = 0.0
+    rowindex = 0
+    while rowindex < rowcount:
+        colindex = 0
+        while colindex < colcount:
+            if rowindex == colindex:
+                sum_value += input_matrix.iloc[rowindex].iloc[colindex]
+                break                
+            colindex += 1
+        rowindex += 1
+    return sum_value
