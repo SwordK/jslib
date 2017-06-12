93fd8f982ea929e65212ca2828bd6e6fad2a7478
wangyijian <wangyijian0525@gmail.com>
2017-06-12 17:31:26 +0800

    [MOD] 修改 拼写错误

--- python/jslib/__init__.py
+++ python/jslib/__init__.py
@@ -6,6 +6,6 @@ import jslib.math_matrix
 
 def get_versions():
     dict_versions = {}
-    dict_versions['dateTime'] = jslib.date_time.get_versions()
+    dict_versions['date_time'] = jslib.date_time.get_versions()
     dict_versions['math_matrix'] = jslib.math_matrix.get_versions()
     return dict_versions
